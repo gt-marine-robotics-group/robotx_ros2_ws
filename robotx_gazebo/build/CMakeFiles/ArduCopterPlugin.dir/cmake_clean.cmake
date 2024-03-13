ca97724f18b8a151350a39f46c24f5c82b3f5a5c
hchardin3 <hchardin3@gatech.edu>
2024-03-13 16:03:13 -0400

    gazebo

---
+++ robotx_gazebo/build/CMakeFiles/ArduCopterPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ArduCopterPlugin.dir/src/ArduCopterPlugin.cc.o"
+  "CMakeFiles/ArduCopterPlugin.dir/src/ArduCopterPlugin.cc.o.d"
+  "libArduCopterPlugin.pdb"
+  "libArduCopterPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArduCopterPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
